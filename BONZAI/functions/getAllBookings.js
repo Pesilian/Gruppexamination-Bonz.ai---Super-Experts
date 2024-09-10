53e2e4683c2685bf9f1f707bad2e5ec2cc4f54b7
dylanR96 <dylan.ravenson@gmail.com>
2024-09-10 19:56:41 +0200

    Able to scan content and return Items from table bookings, response returns message if no bookings

--- BONZAI/functions/getAllBookings.js
+++ BONZAI/functions/getAllBookings.js
@@ -2,11 +2,22 @@ const { db } = require(".././dynamoDb.js");
 
 module.exports.handler = async (event, context) => {
   try {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Welcome" }),
-    };
-    return response;
+    const result = await db.scan({
+      TableName: "bookings",
+    });
+    if (result.Items.length < 0) {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: result.Items }),
+      };
+      return response;
+    } else {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({ message: "There are no current bookings." }),
+      };
+      return response;
+    }
   } catch (error) {
     return console.error(error);
   }
